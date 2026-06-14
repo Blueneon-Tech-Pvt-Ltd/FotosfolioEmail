81080c62bdaa2b5febba103b5a7e3b6b3ae90596
Ares <rochaksulu2002@gmail.com>
2026-06-14 12:07:23 +0545

    fix: extend eslint disable rules for safer type handling in tests

--- src/queues/video/video.processor.spec.ts
+++ src/queues/video/video.processor.spec.ts
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-return */
 import { VideoProcessor } from './video.processor';
 import * as fsPromises from 'node:fs/promises';
 
